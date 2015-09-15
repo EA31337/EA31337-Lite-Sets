b37584e6dddd232bf6eaff2ab8659ebd0a8664ed
kenorb <kenorb@gmail.com>
2015-09-15 22:55:40 +0100

    Organised scripts and config files in a better way.

--- _scripts/run_backtests.sh
+++ _scripts/run_backtests.sh
@@ -1,23 +1,20 @@
-#!/bin/bash
+#!/usr/local/bin/bash -e
 # Initialize variables and settings.
-: ${1?"Usage: $0 (filter)"} || exit 1
+: ${1?"Usage: $0 (dir)"} || exit 1
 ROOT=$(git rev-parse --show-toplevel)
-. $ROOT/_conf/backtest.ini
 
-# Find the set files.
-sets=$(find $ROOT -ipath \*$1\*.set -print0)
+# Find the ini files.
+inis=($(find $ROOT/$1 -name \*.ini))
 
 # Parse configuration and run the tests.
-IFS=',' symbols=($symbols)
-IFS=',' years=($years)
-IFS=',' spreads=($spreads)
-IFS=',' sources=($sources)
-for file in "${sets[@]}"; do
-  for symbol in "${symbols[@]}"; do
-    for year in "${years[@]}"; do
-      for spread in "${spreads[@]}"; do
-        for source in "${sources[@]}"; do
-          run_backtest "$file" $symbol $year $spread $source
+for ini in ${inis[@]}; do
+  . <(grep = "$ini" | sed "s/;/#/g") # Load ini values.
+  dir=$(dirname $ini)
+  for deposit in ${deposits[@]}; do
+    for year in ${years[@]}; do
+      for source in ${sources[@]}; do
+        for spread in ${spreads[@]}; do
+          run_backtest "$dir" $setfile $mode $symbol $deposit $year $spread $source 
         done
       done
     done
