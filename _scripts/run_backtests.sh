0806133c078df96414d8f7e30b2aa5c7c3584a95
kenorb <kenorb@gmail.com>
2015-10-04 22:44:18 +0100

    Improved syntax for finding ini files and added additional -D parameter.

--- _scripts/run_backtests.sh
+++ _scripts/run_backtests.sh
@@ -6,18 +6,15 @@ cd -P -- "$(dirname -- "$0")" && pwd -P
 ROOT="$(git rev-parse --show-toplevel)"
 type run_backtest || run_backtest() { echo run_backtest $*; }
 
-# Find the ini files.
-inis=($(find "$ROOT/$1" -name \*.ini))
-
-# Parse configuration and run the tests.
-for ini in ${inis[@]}; do
-  . <(grep = "$ini" | sed "s/;/#/g") # Load ini values.
-  dir=$(dirname $ini)
+# Find, parse configuration and run the tests.
+find "$ROOT/$1" -type f -name "*.ini" -print0 | while IFS= read -r -d '' file; do
+  . <(grep = "$file" | sed "s/;/#/g") # Load ini values.
+  dir="$(dirname "$file")"
   for deposit in ${deposits[@]}; do
     for year in ${years[@]}; do
       for bt_source in ${bt_sources[@]}; do
         for spread in ${spreads[@]}; do
-          run_backtest -r "$dir" -f $setfile -n $name -p $pair -d $deposit -y $year -s $spread -b $bt_source
+          run_backtest -f $setfile -n $name -p $pair -d $deposit -y $year -s $spread -b $bt_source -D "$dir"
         done
       done
     done
