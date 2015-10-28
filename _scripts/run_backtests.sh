162e07f0f2d3354379fb0017ae163d7c93bff1b8
d-a-t-a-d-i-t <d-a-t-a-d-i-t@users.noreply.github.com>
2015-10-28 13:42:59 +0100

    Generate concatenated report file into backtest set directory.

--- _scripts/run_backtests.sh
+++ _scripts/run_backtests.sh
@@ -7,7 +7,7 @@ ROOT="$(git rev-parse --show-toplevel)"
 type run_backtest || run_backtest() { echo run_backtest $*; }
 
 # Find, parse configuration and run the tests.
-find "$ROOT/$1" -type f -name "*.ini" -print0 | while IFS= read -r -d '' file; do
+find "$ROOT/$1" -type f -name "test.ini" -print0 | while IFS= read -r -d '' file; do
   . <(grep = "$file" | sed "s/;/#/g") # Load ini values.
   dir="$(dirname "$file")"
   for deposit in ${deposits[@]}; do
@@ -15,8 +15,12 @@ find "$ROOT/$1" -type f -name "*.ini" -print0 | while IFS= read -r -d '' file; d
       for bt_source in ${bt_sources[@]}; do
         for spread in ${spreads[@]}; do
           run_backtest.sh -f $setfile -n $name -p $pair -d $deposit -y $year -s $spread -b $bt_source -D "$dir"
+	  find ~/.wine/drive_c -type f -name Report.htm -exec html2text {} >> "$dir/README.md.new" ';'
         done
       done
     done
   done
+
+  # Replace the old concatenated reports with the new one
+  mv "$dir/README.md.new" "$dir/README.md"
 done
