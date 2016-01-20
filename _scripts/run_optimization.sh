85bf0027a1a4e74936ebf842c4a5d107d87abb92
kenorb <kenorb@gmail.com>
2016-01-20 23:01:42 +0000

    Added optional argument from CLI for _scripts.

--- _scripts/run_optimization.sh
+++ _scripts/run_optimization.sh
@@ -13,7 +13,7 @@ find "$ROOT/$1" -type f -name "test.ini" -print0 | while IFS= read -r -d '' file
   find "$ROOT/$1" -type f -name "*$2*.rules" -print0 | while IFS= read -r -d '' rule_file; do
     report_base="$(basename "${rule_file%.*}")"
     report_name="${report_base}--${pair}-${deposit}${currency}-${year}year-${spread}spread-${bt_source}-optimization-test"
-    run_backtest.sh -v -o -t -e $name -f "$dir/$setfile" -c $currency -p $pair -d $deposit -y $year -s $spread -b $bt_source -r "$report_name" -i "$rule_file" -D "$dir/_optimization_results"
+    run_backtest.sh $3 -v -o -t -e $name -f "$dir/$setfile" -c $currency -p $pair -d $deposit -y $year -s $spread -b $bt_source -r "$report_name" -i "$rule_file" -D "$dir/_optimization_results"
     push_report.sh "Optimization-$report_base" "Optimization results: $report_name"
   done
   gen_report.sh "$dir/_optimization_results" "$OUT"
