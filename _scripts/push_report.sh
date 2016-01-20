fc22eba611a0b72409a1552610c5c9c91d495b78
kenorb <kenorb@gmail.com>
2016-01-20 23:18:20 +0000

    Checkout from origin/master.

--- _scripts/push_report.sh
+++ _scripts/push_report.sh
@@ -8,7 +8,7 @@ MSG="$2"
 
 cd "$ROOT"
 git add -vA *.txt *.gif
-git checkout -mB "$CHK" master
+git checkout -mB "$CHK" origin/master
 git commit -vm "$MSG" -a && git push -fv origin "$CHK"
 times
 echo "$0 done."
