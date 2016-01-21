eb24c9699939b3e045a64379becdc9d52336030a
kenorb <kenorb@gmail.com>
2016-01-21 23:40:45 +0000

    Backtest results: EURUSD-2000USD-2014year-10spread-DS

--- _scripts/gen_report.sh
+++ _scripts/gen_report.sh
@@ -9,7 +9,7 @@ ROOT="$(git rev-parse --show-toplevel)"
 [ -d "$DIR" ]
 
 > "$DIR/$OUT"
-find "$DIR" -type f -name "$FILTER*test.txt" -print0 | sort -z | while IFS= read -r -d '' report; do
+find "$DIR" -type f -name "*$FILTER*test.txt" -print0 | sort -z | while IFS= read -r -d '' report; do
   file=$(basename "$report")
   name=$(echo "$file" | sed "s/-/ /g")
   gif="$(basename "${file%.*}.gif")"
