29733b0f476e7f22c6f3d2167b274b328376c6b7
EA Tester <kenorb+ea@gmail.com>
2016-01-23 14:06:44 +0000

    Test results for: Optimization-Strategy-Bands-Trailing-Stop-Profit

--- _scripts/push_report.sh
+++ _scripts/push_report.sh
@@ -5,10 +5,10 @@ type git
 ROOT="$(git rev-parse --show-toplevel)"
 CHK="$1"
 MSG="$2"
-AUTHOR="Tester <kenorb+ea@gmail.com>"
+AUTHOR="EA Tester <kenorb+ea@gmail.com>"
 
 cd "$ROOT"
 git checkout -mB "$CHK" origin/master
 git add -vA *.txt *.gif *.md
-git commit --theirs -vm "$MSG" --author="$AUTHOR" -a && git push -fv origin "$CHK"
+git commit -vm "$MSG" --author="$AUTHOR" -a && git push -fv origin "$CHK"
 echo "$0 done."
