20a62bae23924f987732482fa671e245fdb0e884
kenorb <kenorb@users.noreply.github.com>
2016-05-11 15:14:12 +0100

    Added support for number of digits for the current symbol prices.

--- _scripts/run_main_backtest.sh
+++ _scripts/run_main_backtest.sh
@@ -13,6 +13,6 @@ find "$ROOT" -type f -name "test.ini" -path "*$1*" -print0 | sort -z | while IFS
   dirname=$(basename "$dir")
   base_name="${pair}-${deposit}${currency}-${year}year-${spread}spread-${bt_source}"
   report_name="${pair}-${deposit}${currency}-${year}year-${spread}spread-${bt_source}-main-test"
-  run_backtest.sh ${@:2} -v -t -e $name -f "$dir/$setfile" -c $currency -p $pair -d $deposit -y $year -s $spread -b $bt_source -r "$report_name" -D "$dir"
+  run_backtest.sh ${@:2} -v -t -e $name -f "$dir/$setfile" -c $currency -p $pair -d $deposit -D $digits -y $year -s $spread -b $bt_source -r "$report_name" -O "$dir"
 done
 echo "$0 done."
