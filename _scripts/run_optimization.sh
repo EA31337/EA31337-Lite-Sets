3322a84b19b56d11d88a59597ab477ae2f78e78a
kenorb <kenorb@users.noreply.github.com>
2016-07-01 01:06:13 +0100

    Corrected run_optimization.sh to load rules init file

--- _scripts/run_optimization.sh
+++ _scripts/run_optimization.sh
@@ -5,6 +5,7 @@ cd -P -- "$(dirname -- "$0")" && pwd -P
 [ "$TRACE" ] && set -x
 ROOT="$(git rev-parse --show-toplevel)"
 PATH="$PATH:$ROOT/_scripts:$ROOT/_VM/scripts"
+RULES_INIT="$ROOT/_rules/.init.rules.inc"
 OUT="README.md"
 [ "$TRACE" ] && set -x
 
@@ -15,10 +16,10 @@ find "$ROOT" -type f -name "test.ini" -print0 | sort -z | while IFS= read -r -d 
   find "$ROOT" -type f '(' -name "*$1*.rule" -o -name "*$1*.rules" ')' -print0 | while IFS= read -r -d '' rule_file; do
     report_base="$(basename "${rule_file%.*}")"
     report_name="${report_base}--${pair}-${deposit}${currency}-${year}year-${spread}spread-${bt_source}-optimization-test"
-    run_backtest.sh ${@:2} -v -o -t -e $name -f "$dir/$setfile" -c $currency -p $pair -d $deposit -D $digits -y $year -s $spread -b $bt_source -r "$report_name" -i "$rule_file" -O "$dir/_optimization_results"
+    run_backtest.sh ${@:2} -v -o -t -e $name -f "$dir/$setfile" -c $currency -p $pair -d $deposit -D $digits -y $year -s $spread -b $bt_source -r "$report_name" -i "$RULES_INIT" -i "$rule_file" -O "$dir/_optimization_results"
     [ "$TEST" ] && continue
     # gen_report.sh "$dir/_optimization_results" "$OUT"
-    push_report.sh "Optimization-$report_base" "Optimization results: $report_name"
+    [ "$PUSH" ] && push_report.sh "Optimization-$report_base" "Optimization results: $report_name"
   done
 done
 echo "$0 done."
