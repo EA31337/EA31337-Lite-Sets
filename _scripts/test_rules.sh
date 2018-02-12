2a44ca57ab5cffce8b4a1b1f086b60beb4dd6858
kenorb <kenorb@users.noreply.github.com>
2018-02-12 22:33:11 +0000

    Corrects test_rules.sh script

--- _scripts/test_rules.sh
+++ _scripts/test_rules.sh
@@ -3,17 +3,18 @@
 set -e
 [ "$TRACE" ] && set -x
 ROOT="$(git rev-parse --show-toplevel)"
-SETFILE="$ROOT/EURUSD/default/2000USD/10-spread/5-digits/2014/EA31337-Lite.set"
-TESTER_INI="$ROOT/_VM/conf/mt4-tester.ini"
-EA_INI="$ROOT/_VM/conf/ea.ini"
+SETFILE="$ROOT/EURUSD/default/10000USD/20-spread/5-digits/2014-2016/EA31337-Lite.set"
+VM_DIR="$(find "$ROOT" -type d -name _VM -print -quit)"
+TESTER_INI="$VM_DIR/conf/mt4-tester.ini"
+EA_INI="$VM_DIR/conf/ea.ini"
 cp -f "${SETFILE}" "${SETFILE}.bak" && SETFILE="${SETFILE}.bak"
 cp -f "${TESTER_INI}" "${TESTER_INI}.bak" && TESTER_INI="${TESTER_INI}.bak"
 cp -f "${EA_INI}" "${EA_INI}.bak" && EA_INI="${EA_INI}.bak"
 
 cd "$ROOT"
-. ./_VM/scripts/.funcs.inc.sh
-. ./_VM/scripts/.vars.inc.sh
-. ./_rules/.init.rules.inc
+. "$VM_DIR"/scripts/.funcs.inc.sh
+. "$VM_DIR"/scripts/.vars.inc.sh
+. "$ROOT"/_rules/.init.rules.inc
 
 find "$ROOT" -type f -name "*$1*.rule" -print0 | sort -z | while IFS= read -r -d '' rule_file; do
   echo "Testing $(basename ${rule_file} .rule)..."
